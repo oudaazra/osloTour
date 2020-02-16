c6e013a525f661c77cf3a3b4287e42f4b0a2683c
oudaazra <48717549+oudaazra@users.noreply.github.com>
2020-02-16 23:11:10 +0100

    initiales commit

---
+++ src/components/tourData.js
@@ -0,0 +1,35 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Oslo",
+    img: "./img/Astrup.jpg",
+    name: "Astrup Fearnley Museet",
+    info:
+      "The Astrup Fearnley Collection is a collection of modern and contemporary art counted among the most significant of its kind in Northern Europe. The museum complements works from the permanent collection by rotating exhibitions with internationally renowned artists The collection focuses on acquiring individual, ground-breaking works. Young American artists used to dominate, but significant voices from Europe, Brazil, Japan, China, and India are now included"
+  },
+  {
+    id: 2,
+    city: "Oslo",
+    img: "./img/cityhall.jpg",
+    name: "Oslo City Hall",
+    info:
+      "Inaugurated in 1950, Oslo City Hall is the city's administrative body and the seat of the City Council .The building has been decorated by great Norwegian art from 1900-1950, with motifs from Norwegian history, culture and working life. Carillon concerts on the first Wednesday of every month at 1 pm."
+  },
+  {
+    id: 3,
+    city: "Oslo",
+    img: "./img/nobelpeace.jpg",
+    name: "Nobel Peace Center",
+    info:
+      "The Nobel Peace Center is the museum of the Nobel Peace Prize. The Center shows an exciting combination of temporary and permanent exhibitions that explore themes related to peace, war and conflict resolution.Explore the history and background of the Peace Prize laureates, presented on digital screens in the beautiful Nobel Field, surrounded by thousands of small lights. Also, don't miss the magical book about Alfred Nobel's life and work."
+  },
+  {
+    id: 4,
+    city: "Oslo",
+    img: "./img/Ekebergparken.jpg",
+    name: "Ekebergparken Sculpture Park",
+    info:
+      "At Ekebergparken Sculpture Park, visitors can partake in experiences involving both art, history and nature. The park is large, with woods, established paths and art. All sculptures are of high quality and are representative of European art history of the last 130 years. So far 31 sculptures have been erected. These will in coming years be accompanied by increasingly more artworks. The combination of Ekeberg’s long history, the beautiful nature and the splendid sculptures offers visitors a triple experience. "
+  },
+  
+];
